a07d09648429929c69e4008d2283676260b17db9
Matheus Betim <mbetim47@gmail.com>
2022-12-15 21:42:22 -0300

    feat: prevent generating the same number as the last one refactor: move the logic to a hook

--- src/screens/Game.tsx
+++ src/screens/Game.tsx
@@ -1,25 +1,47 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useState } from "react";
+import Constants from "expo-constants";
+import React, { useEffect, useRef, useState } from "react";
 import { Text, TouchableWithoutFeedback, View } from "react-native";
 import { RootStackParamList } from "../../App";
+import { Deck } from "../@types/Deck";
 import { Container } from "../components/Container";
-import Constants from "expo-constants";
 import { PlayingCard } from "../components/PlayingCard";
 
 type GameProps = NativeStackScreenProps<RootStackParamList, "Game">;
 
+const useRandomNumber = (deck: Deck) => {
+  const isFirstRender = useRef(true);
+
+  const [generatedNumbers, setGeneratedNumbers] = useState<number[]>([]);
+  const currentNumber = generatedNumbers[generatedNumbers.length - 1];
+
+  const generateRandomNumber = (lastNumber?: number): number => {
+    const newNumber = Math.floor(Math.random() * deck.cards.length);
+    return lastNumber === newNumber ? generateRandomNumber(lastNumber) : newNumber;
+  };
+
+  const newNumber = () => {
+    const newNumber = generateRandomNumber(currentNumber);
+    setGeneratedNumbers((prev) => [...prev, newNumber]);
+  };
+
+  useEffect(() => {
+    if (!isFirstRender.current) return;
+
+    newNumber();
+    isFirstRender.current = false;
+  }, []);
+
+  return { currentNumber, generatedNumbers, newNumber };
+};
+
 export const Game: React.FC<GameProps> = ({ route }) => {
   const { deck } = route.params;
-  const generateRandomNumber = () => Math.floor(Math.random() * deck.cards.length);
-
-  const [generatedNumbers, setGeneratedNumbers] = useState([generateRandomNumber()]);
-  const currentNumber = generatedNumbers[generatedNumbers.length - 1];
+  const { currentNumber, newNumber, generatedNumbers } = useRandomNumber(deck);
 
   return (
     <Container style={{ paddingTop: Constants.statusBarHeight }}>
-      <TouchableWithoutFeedback
-        onPress={() => setGeneratedNumbers((prev) => [...prev, generateRandomNumber()])}
-      >
+      <TouchableWithoutFeedback onPress={() => newNumber()}>
         <View className="flex-1">
           <Text className="text-center text-white text-2xl font-bold mt-4">
             Round: {generatedNumbers.length + 1}
@@ -29,7 +51,7 @@ export const Game: React.FC<GameProps> = ({ route }) => {
             <PlayingCard number={currentNumber + 1} />
 
             <Text className="mt-2 text-center text-xl text-white font-bold">
-              {deck.cards[currentNumber].name}
+              {deck.cards[currentNumber]?.name}
             </Text>
           </View>
         </View>
